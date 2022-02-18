c08015f34bb50084f14d83c5e032417b57f96ba9
Arseniy <73026795+arseniy89@users.noreply.github.com>
2022-02-19 02:41:21 +0300

    Update tic-tac-toe.py

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -13,7 +13,7 @@ def reading(s):
             if b[p] == ' ' and p >= 0:
                 b[p] = 'o' if (s%2) else 'x'
                 cout(); return
-        except: continue
+        except: pass
 
 
 def check():
